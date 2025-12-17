960b6f26ca2efc627096b8d8cff19319e1409253
MajorAchilles <amlanjyoti.s@gmail.com>
2025-12-17 13:40:50 +0530

    feat: update MemCacheClient to include maxSizeInBytes option and enforce size limit on set method; bump version to 1.0.3

--- src/cacheClient/MemCacheClient.ts
+++ src/cacheClient/MemCacheClient.ts
@@ -4,11 +4,17 @@ import { CacheClient } from './CacheClient';
 export type MemCacheClientOptions = {
   recordTTLSeconds?: number;
   maxItems?: number;
+  maxSizeInBytes?: number;
 };
 
 const defaultMemCacheClientOptions: MemCacheClientOptions = {
   recordTTLSeconds: 60,
   maxItems: 1000,
+  maxSizeInBytes: 5242880, // 5 MiB
+};
+
+const getApproximateStringSizeInBytes = (str: string): number => {
+  return str.length * 2; // Approximate size in bytes (assuming UTF-16)
 };
 
 export class MemCacheClient implements CacheClient {
@@ -16,12 +22,14 @@ export class MemCacheClient implements CacheClient {
   private static instance: MemCacheClient;
   private isReady: boolean = false;
   private recordTTLSeconds: number;
+  private maxSizeInBytes: number;
 
   private constructor(memCacheClientOptions?: Partial<MemCacheClientOptions>) {
     this.cache = new LRUCache<string, { value: string; expiresAt: number }>({
       max: memCacheClientOptions?.maxItems || defaultMemCacheClientOptions.maxItems!,
     });
     this.recordTTLSeconds = memCacheClientOptions?.recordTTLSeconds || defaultMemCacheClientOptions.recordTTLSeconds!;
+    this.maxSizeInBytes = memCacheClientOptions?.maxSizeInBytes || defaultMemCacheClientOptions.maxSizeInBytes!;
     this.isReady = true;
   }
 
@@ -52,6 +60,9 @@ export class MemCacheClient implements CacheClient {
   }
 
   public async set(key: string, value: string): Promise<void> {
+    if (getApproximateStringSizeInBytes(value) > this.maxSizeInBytes) {
+      return Promise.reject(new Error('Item size exceeds maximum allowed size for MemCacheClient'));
+    }
     this.cache.set(key, { value, expiresAt: Date.now() + (this.recordTTLSeconds * 1000) });
     return Promise.resolve();
   }
